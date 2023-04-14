1eceec5606a5687fe303fd361a81e4ba1fecd8ab
houssembouraoui <houssembouraoui6@gmail.com>
2023-04-14 17:11:34 +0100

    fixed some issues

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -6,10 +6,10 @@ const Contact = () => {
   const [subject, setSubject] = useState("");
   const [message, setMessage] = useState("");
 
-  (function () {
-    emailjs.init("xpnFy2CseDZ24U7pE");
-  })();
-  
+  // (function () {
+  //   emailjs.init("xpnFy2CseDZ24U7pE");
+  // })();
+
   return (
     <section id="contact" className="contact">
       <div className="container" data-aos="fade-up">
@@ -23,7 +23,7 @@ const Contact = () => {
             style={{ border: "0", width: "100%", height: "270px" }}
             src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d12097.433213460943!2d-74.0062269!3d40.7101282!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xb89d1fe6bc499443!2sDowntown+Conference+Center!5e0!3m2!1smk!2sbg!4v1539943755621"
             frameborder="0"
-            allowfullscreen
+            allowFullScreen
           ></iframe>
         </div>
 
